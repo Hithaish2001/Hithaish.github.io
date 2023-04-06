c392d9bfe691852f8abf70dffdbf443cf862b000
Hithaish <shetty9606@gmail.com>
2023-04-06 20:40:37 +0530

    started navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,13 @@
+
 const Navbar=()=>{
     return(
-        <div className="nav bg-black">
-            
+        <div className="nav bg-black h-20">
+           <ul className="flex-col md:flex-row">
+           <li className="list-none text-white"><a href="#" id="home">HOME</a></li>
+            <li className="list-none text-white"><a href="#" id="about">ABOUT</a></li>
+            <li className="list-none text-white"><a href="#" id="projects">PROJECTS</a></li>
+            <li className="list-none text-white"><a href="#" id="contact">CONTACT ME</a></li>
+           </ul>
         </div>
     )
 }
