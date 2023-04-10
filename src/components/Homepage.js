742cd03d06e6580a8228627c7aef01cdf433b7f3
Hithaish <shetty9606@gmail.com>
2023-04-11 00:11:53 +0530

    small design changes

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -7,7 +7,7 @@ const Home=()=>{
                     <h1 className="text-2xl font-semibold">Hello, My name is</h1>
                     <h1 className="text-7xl font-bold">Hithaish</h1>
                 </div>
-                <div className="image flex ml-96 mt-36 mr-20">
+                <div className="image flex ml-96 mt-52 mr-20">
                     <img src={logo} alt="" style={{width: "700px"}}/>
                 </div>
             </div>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -8,25 +8,25 @@ const Navbar=()=>{
     const [nav, setNav]=useState(false)
     const handleclick=()=>setNav(!nav);
     return(
-        <nav className="nav fixed bg-black w-full h-24 flex text-white justify-between ">
+        <nav className="nav fixed bg-transparent w-full h-24 flex text-black justify-between drop-shadow-2xl">
             <div className="logo flex">
-                <h1 className=" pt-8 pl-10 text-2xl">Portfolio.</h1>
+                <h1 className=" pt-8 pl-10 text-2xl font-semibold">Portfolio.</h1>
             </div>
-           <div className=" pr-8 pt-12 hidden md:flex">
-            <li className="list-none px-5"><a href="#home" id="home">HOME</a></li>
-            <li className="list-none px-5"><a href="#about" id="about">ABOUT</a></li>
-            <li className="list-none px-5"><a href="#projects" id="projects">PROJECTS</a></li>
-            <li className="list-none px-5"><a href="#contact" id="contact">CONTACT  ME</a></li>
-            <li className="list-none px-5">TOGGLE</li>
+           <div className=" pr-8 pt-12 hidden md:flex font-medium ">
+            <li className="links  list-none px-5"><a href="#home" id="home">HOME</a></li>
+            <li className="links  list-none px-5"><a href="#about" id="about">ABOUT</a></li>
+            <li className="links  list-none px-5"><a href="#projects" id="projects">PROJECTS</a></li>
+            <li className="links  list-none px-5"><a href="#contact" id="contact">CONTACT  ME</a></li>
+            <li className="links  list-none px-5">TOGGLE</li>
            </div>
 
 
            {/* mobile navbar */}
-           <div onClick={handleclick} className="md:hidden z-10">
+           <div onClick={handleclick} className="md:hidden z-10 text-zinc-500">
                 {!nav ? <FaBars/> : <FaTimes/>}
            </div>
 
-           <ul className={!nav ? "hidden" : "absolute top-0 left-0 w-full h-screen bg-black flex flex-col justify-center items-center"}>
+           <ul className={!nav ? "hidden" : "absolute text-zinc-500 top-0 left-0 w-full h-screen bg-neutral-900 flex flex-col justify-center items-center"}>
            <li className="list-none py-4"><a href="#home" id="home">HOME</a></li>
             <li className="list-none py-4"><a href="#about" id="about">ABOUT</a></li>
             <li className="list-none py-4"><a href="#projects" id="projects">PROJECTS</a></li>
@@ -35,18 +35,18 @@ const Navbar=()=>{
            </ul>
 
            {/* social accout reach-outs */}
-           <div className="social fixed flex flex-col left-0 top-[350px]">
+           <div className="social hidden lg:flex fixed flex-col left-0 top-[350px]">
             <ul>
-                <li className="bg-black w-40 h-14 flex justify-between items-center ml-[-100px] hover:ml-[0px] shadow-2xl shadow-gray-400  duration-500">
+                <li className="bg-neutral-900 w-40 h-14 flex justify-between items-center ml-[-100px] hover:ml-[0px] shadow-2xl shadow-gray-400  duration-700">
                     <a className="flex w-36 text-white justify-between items-center text-xl pl-5" href="https://www.linkedin.com/in/hithaish-n-shetty-698a45227">LinkedIn <BsLinkedin size={35}/></a>
                 </li>
-                <li className="bg-black w-40 h-14 flex justify-between items-center ml-[-100px] hover:ml-[0px] shadow-2xl shadow-gray-400  duration-500">
+                <li className="bg-neutral-900 w-40 h-14 flex justify-between items-center ml-[-100px] hover:ml-[0px] shadow-2xl shadow-gray-400  duration-700">
                     <a className="flex w-36 text-white justify-between items-center text-xl pl-5" href="https://github.com/Hithaish2001">Github <FaGithub size={35}/></a>
                 </li>
-                <li className="bg-black w-40 h-14 flex justify-between items-center ml-[-100px] hover:ml-[0px] shadow-2xl shadow-gray-400  duration-500">
+                <li className="bg-neutral-900 w-40 h-14 flex justify-between items-center ml-[-100px] hover:ml-[0px] shadow-2xl shadow-gray-400  duration-700">
                     <a className="flex w-36 text-white justify-between items-center text-xl pl-5" href="/">Email <GrMail size={35}/></a>
                 </li>
-                <li className="bg-black w-40 h-14 flex justify-between items-center ml-[-100px] hover:ml-[0px] shadow-2xl shadow-gray-400  duration-500">
+                <li className="bg-neutral-900 w-40 h-14 flex justify-between items-center ml-[-100px] hover:ml-[0px] shadow-2xl shadow-gray-400  duration-700">
                     <a className="flex w-36 text-white justify-between items-center text-xl pl-5" href="https://twitter.com/Hithaish01">Twitter <GrTwitter size={35}/></a>
                 </li>
             </ul>
