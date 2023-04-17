a65e576468e38c5a46d00b8f687394d2a8b10c69
Hithaish <shetty9606@gmail.com>
2023-04-17 16:31:50 +0530

    completed about section

--- src/components/about.js
+++ src/components/about.js
@@ -11,11 +11,11 @@ import tailwind from "../assets/tailwind.png";
 const About=()=> {
 
   
-  const [selected, setSelected] = useState(null);
+  const [selected, setSelected] = useState('skills');
 
   const handleClick = (e) => {
     if (selected === e) {
-      setSelected(null);
+      setSelected(selected);
     } else {
       setSelected(e);
     }
@@ -23,7 +23,7 @@ const About=()=> {
 
   return (
 
-    <div name="about" className='w-screen h-screen flex justify-center items-center flex-col bg-white'>
+    <div name="about" className='w-screen h-screen flex justify-center items-center flex-col bg-white font-[470]'>
         <p className='text-5xl font-semibold text-center pb-16'>About Me</p>
         <div name="" className='flex w-full justify-center items-start pl-32'>
             <div className="pic  w-72 h-96 flex flex-none  hover:-translate-y-5 hover:translate-x-2 duration-300  left-[17rem] ">
@@ -32,14 +32,14 @@ const About=()=> {
             <div className="selfD w-3/5  text-lg font-normal items-start pl-20">
                 <p>Hello guys, I'm Hithaish. I am interested in developing web applications and exploring new technologies. Currently I'm working on reactjs projects for learning</p>
                 <ul className='subnav pt-5 flex space-x-8 font-medium cursor-pointer uppercase '>
-                  <li className={`navelements active-navelements ${selected === 'skills' ? 'bg-gray-300' : ''}`} onClick={() => handleClick('skills')}>Skills</li>
-                  <li className={`${selected === 'skills' ? 'bg-gray-300' : ''}`}  onClick={() => handleClick('education')}>Education</li>
-                  <li className={`${selected === 'skills' ? 'bg-gray-300' : ''}`}  onClick={() => handleClick('experience')}>Experirence</li>
+                  <li className={`navelements  ${selected === 'skills' ? 'active-navelements' : ''}`} onClick={() => handleClick('skills')}>Skills</li>
+                  <li className={`navelements  ${selected === 'education' ? 'active-navelements' : ''}`}  onClick={() => handleClick('education')}>Education</li>
+                  <li className={`navelements  ${selected === 'experience' ? 'active-navelements' : ''}`}  onClick={() => handleClick('experience')}>Experirence</li>
                 </ul>
                 <div className='mainContainer'>
                     
                     {selected === 'skills' && (
-                      <div name="skills"  className={`content active-content flex h-72  relative top-14 -left-5`}>
+                      <div name="skills"  className={`content active-content flex h-72  relative top-6 -left-5`}>
                         <ol className='flex justify-center items-center pb-8 font-medium'>
                           <li><img src={html} alt='HTMl' className='w-44 px-3 animate-animation-bounce drop-shadow-xl'/><p className='text-black pt-3 text-center font-sans'>HTML</p></li>
                           <li><img src={css} alt='css' className='w-44 px-3 animate-animation-bounce drop-shadow-xl'/><p className='text-black pt-3 text-center font-sans'>CSS</p></li>
@@ -52,21 +52,21 @@ const About=()=> {
 
                     
                     {selected=== 'education' && (
-                      <div name='education' className={``}>
-                        <ol className='list-disc flex flex-col space-y-5 font-[450]'>
-                          <li><span className='font-medium'>Graduating 2023</span><span className='flex'>B.E IN COMPUTER SCIENCE AND ENGINEERING<span className='pl-14'>CGPA: 8.03</span></span></li>
-                          <li><span className='font-medium'>Graduated 2019</span><span className='flex'>PUC<span className='pl-[400px]'>Percentage: 89.5</span></span></li>
-                          <li><span className='font-medium'>Graduated 2017</span><span className='flex'>SSLC<span className='pl-[400px]'>Percentage: 84.96</span></span></li>
+                      <div name='education' className='pt-7 pl-4'>
+                        <ol className='list-disc flex flex-col space-y-5 font-[470]'>
+                          <li><span className='font-medium'>Graduating 2023</span><span className='flex'>B.E IN COMPUTER SCIENCE AND ENGINEERING &nbsp; | &nbsp; CGPA: 8.03</span></li>
+                          <li><span className='font-medium'>Graduated 2019</span><span className='flex flex-col'>PUC &nbsp; | &nbsp; Percentage: 89.5</span></li>
+                          <li><span className='font-medium'>Graduated 2017</span><span className='flex flex-col'>SSLC &nbsp; | &nbsp; Percentage: 84.96</span></li>
                         </ol>
                       </div>
                     )}
 
 
                     {selected === 'experience' && (
-                      <div name='experience' className={``}>
+                      <div name='experience' className='pt-7 pl-4'>
                         <ol className='list-none font-[470]'>
-                        <p className='font-semibold'> Full Stack Developer Intern <span className='pl-52'>Aug 2022-Sep 2022</span></p>
-                        <li className='list-disc'>I have done one month internship at Thaniya Technologies IT Solutions LLP, Mangaluru</li>
+                        <p className='font-semibold pb-2'> Full Stack Developer Intern (Aug 2022-Sep 2022)</p>
+                        <li className='list-disc'>I have done one month internship at <a className='font-medium underline' href='https://thaniyatech.com/'>Thaniya Technologies IT Solutions LLP</a>, Mangaluru.</li>
                         <li className='list-disc'>One of the most valuable skills I had learned are project planning & management and the ability to speak with people in a professional setting.</li>
                         <li className='list-disc'>Developed a full-stack application for an online bookseller. It Primarily enables the admin to manage the program by keeping track of orders, book availability, managing users, and other admin's actions.</li>
                         </ol>
