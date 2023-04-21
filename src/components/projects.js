e90c6800f0a32b35a151f75aa1b987bb96a6010d
Hithaish <shetty9606@gmail.com>
2023-04-22 00:17:14 +0530

    completed with projects section

--- src/components/projects.js
+++ src/components/projects.js
@@ -4,18 +4,52 @@ import news from '../assets/newszone-min.png'
 import textutil from '../assets/textanalyzer.png'
 import tictactoe from '../assets/tictactoe.png'
 import Wcounter from '../assets/wordcounter.png'
-import Carousel  from './sample'
+import cab from '../assets/cab.jpg'
 
 const Project=()=>{
 
 
-    const images=[news, textutil,bookstore,tictactoe,Wcounter
-      // {url: news},
-      // {url: textutil},
-      // {url: bookstore},
-      // {url: tictactoe},
-      // {url: Wcounter},
+    const images=[
+      {id:1,url:'https://github.com/Hithaish2001/NewsZone',images: news, title:'Newsapp(ReactJS)'},
+      {id:2,url:'https://github.com/Hithaish2001/TextUtils ',images: textutil, title:'Text Analyser(ReactJS)'},
+      {id:3,url:'https://github.com/Hithaish2001/onlilne-book-store',images: bookstore, title:'online Book Store(php)'},
+      {id:4,url:'https://github.com/Hithaish2001/TicTacToe ',images: tictactoe, title:'TicTacToe(js)'},
+      {id:5,url:'https://github.com/Hithaish2001/word-counter',images: Wcounter, title:'Word and Character counter(js)'},
+      {id:6,url:'https://github.com/Hithaish2001/DBMS-project',images: cab, title:'Cab Management(DBMS)'}
     ];
+
+  return (
+    
+    <div name="main" className='w-screen h-screen justify-center items-center flex flex-col'>
+        <p className='text-5xl font-semibold text-center font-mono py-7'>PROJECTS</p>
+
+
+         <div className=' w-full h-full justify-center items-center grid grid-cols-3 pl-44'>
+            {images.map((items)=>(
+              <div key={items.id} className='w-96 h-[220px] relative  rounded-xl shadow-2xl shadow-gray-700 group overflow-hidden'>
+                
+                  <a href={items.url} rel='noreferrer' target='_blank' className='relative '>
+                    <img src={items.images} alt="" className='w-full h-full rounded-xl opacity-100 group-hover:opacity-75 relative z-10 scale-100 group-hover:scale-105 transition-all duration-500 ease-out origin-top'/>
+                  </a>
+
+                  <div className='w-full h-full absolute bg-black/60 opacity-0 hover:opacity-100 justify-center items-center flex text-xl rounded-xl z-50 top-0 left-0 flex-col '>
+                    <div className='text-white opacity-0 group-hover:opacity-100 scale-[1.3]  group-hover:scale-[1] transition-all ease-out duration-700'>
+                      {items.title}
+                    </div>
+                  </div>
+                  
+              </div>
+            ))}          
+         </div> 
+          
+    </div>
+  )
+}
+
+export default Project
+
+
+
 
   // const [current,setCurrent]=useState(0)
 
@@ -40,70 +74,44 @@ const Project=()=>{
   //   setCurrent(slideIndex)
   // }
 
-  return (
-    
-    <div name="main" className='w-screen h-screen justify-center items-center flex flex-col'>
-        <p className='text-5xl font-semibold text-center font-mono py-7'>PROJECTS</p>
-
-
-         <div className=' w-full h-full relative justify-center items-center flex '>
-
-
-
-          <div className='carsoul carsoul-center w-[850px] h-[490px] bottom-16 absolute bg-gray-300 rounded-2xl drop-shadow-2xl ring ring-offset-1 ring-gray-400'>
-            <Carousel autoSlide='true'>
-              {images.map((s)=>(<img key={s} src={s} alt="" className='h-[490px] rounded-2xl'/> ))}
-            </Carousel>
-          </div>
-
-           
-         </div> 
-          
-    </div>
-  )
-}
-
-export default Project
+// {/* right button */}
+//            {/* <div className="relative">
+//             <div className='absolute top-[50%] translate-x-[435px] translate-y-[-50%]  text-2xl rounded-full p-2 bg-black/50 text-white cursor-pointer hover:bg-black/75 hover:scale-110 duration-[600ms] ' onClick={next}>
+//               <BsChevronCompactRight size={40}/>
+//             </div>
+//           </div> */}
 
 
 
+ // <div className="absolute flex justify-center items-center ">
+          //   <div name='mainImg' className='w-[790px] h-[490px]  bg-gray-300 rounded-2xl drop-shadow-2xl ring ring-offset-1 ring-gray-300 -translate-y-3 hover:scale-105 duration-500 '>
+          //     <img src={`${slides[current].url}`} alt='' className='h-full rounded-2xl '></img>
+          //   </div>
+          // </div>   
 
 
 
-{/* right button */}
-           {/* <div className="relative">
-            <div className='absolute top-[50%] translate-x-[435px] translate-y-[-50%]  text-2xl rounded-full p-2 bg-black/50 text-white cursor-pointer hover:bg-black/75 hover:scale-110 duration-[600ms] ' onClick={next}>
-              <BsChevronCompactRight size={40}/>
-            </div>
-          </div> */}
-
-
-
-           {/* left button */}
-           {/* <div className='relative '>
-            <div className='absolute top-[50%] -translate-x-[435px] translate-y-[-50%] right-1 text-2xl rounded-full p-2 bg-black/50 text-white cursor-pointer hover:bg-black/75 hover:scale-110 duration-[600ms] ' onClick={previous}>
-               <BsChevronCompactLeft size={40}/>
-             </div>
-           </div>
-               */}
+//            {/* left button */}
+//            {/* <div className='relative '>
+//             <div className='absolute top-[50%] -translate-x-[435px] translate-y-[-50%] right-1 text-2xl rounded-full p-2 bg-black/50 text-white cursor-pointer hover:bg-black/75 hover:scale-110 duration-[600ms] ' onClick={previous}>
+//                <BsChevronCompactLeft size={40}/>
+//              </div>
+//            </div>
+//                */}
             
 
-            {/* dots below image slider */}
-         {/* <div className='flex space-x-3 pt-16'>
-           {slides.map((slide,slideIndex)=>(
-             <div key={slideIndex} onClick={()=>show(slideIndex)} className={`flex text-gray-600 cursor-pointer  hover:text-black `}>
-               <GoPrimitiveDot size={20}/>
-             </div>
-           ))}
-         </div> */}
+//             {/* dots below image slider */}
+//          {/* <div className='flex space-x-3 pt-16'>
+//            {slides.map((slide,slideIndex)=>(
+//              <div key={slideIndex} onClick={()=>show(slideIndex)} className={`flex text-gray-600 cursor-pointer  hover:text-black `}>
+//                <GoPrimitiveDot size={20}/>
+//              </div>
+//            ))}
+//          </div> */}
         
 
 
 
           // in case the above code dosen't work use this for the main image slider( BACKUP CODE )  
 
-          // <div className="absolute flex justify-center items-center ">
-          //   <div name='mainImg' className='w-[790px] h-[490px]  bg-gray-300 rounded-2xl drop-shadow-2xl ring ring-offset-1 ring-gray-300 -translate-y-3 hover:scale-105 duration-500 '>
-          //     <img src={`${slides[current].url}`} alt='' className='h-full rounded-2xl '></img>
-          //   </div>
-          // </div>   
+         
--- src/components/sample.js
+++ src/components/sample.js
@@ -1,44 +1,58 @@
-import { useState, useEffect } from "react"
-import {BsChevronCompactLeft,BsChevronCompactRight} from 'react-icons/bs'
 
- const Carousel=({children: slides,autoSlide = false,autoSlideInterval = 3000,})=> {
-  const [curr, setCurr] = useState(0)
 
-  const prev = () =>
-    setCurr((curr) => (curr === 0 ? slides.length - 1 : curr - 1))
-  const next = () =>
-    setCurr((curr) => (curr === slides.length - 1 ? 0 : curr + 1))
 
-  useEffect(() => {
-    if (!autoSlide) return
-    const slideInterval = setInterval(next, autoSlideInterval)
-    return () => clearInterval(slideInterval)
-  }, [])
-  return (
-    <div className="overflow-hidden relative">
-      <div className="flex transition-transform ease-out duration-500" style={{ transform: `translateX(-${curr * 100}%)` }} >
-        {slides}
-      </div>
+// need to use the below component in other component
 
-      <div className="absolute inset-0 flex items-center justify-between p-1">
-        <button onClick={prev} className="p-1  rounded-full shadow bg-black/80 text-white hover:bg-black  hover:drop-shadow-lg hover:scale-110 duration-500" >
-          <BsChevronCompactLeft size={60} />
-        </button>
+// {/* <Carousel autoSlide='true'>
+// {images.map((s,id)=>(<img key={id} src={s} alt="" className='h-[490px] rounded-2xl'/> ))}
+// </Carousel> */}
 
-        <button onClick={next}  className="p-1 rounded-full shadow bg-black/80 text-white hover:bg-black  hover:drop-shadow-lg hover:scale-110 duration-500">
-        <BsChevronCompactRight size={60} />
-        </button>
-      </div>
 
-      <div className="absolute bottom-4 right-0 left-0">
-        <div className="flex items-center justify-center gap-2">
-          {slides.map((_, i) => (
-            <div key={i} className={`transition-all w-3 h-3 bg-black rounded-full hover:scale-110  ${curr === i ? "p-2" : "bg-opacity-50"}`}/>
-          ))}
-        </div>
-      </div>
-    </div>
-  )
-}
 
-export default Carousel
+//code for image slider 
+
+
+// import { useState, useEffect } from "react"
+// import {BsChevronCompactLeft,BsChevronCompactRight} from 'react-icons/bs'
+
+//  const Carousel=({children: slides,autoSlide = false,autoSlideInterval = 3000,})=> {
+//   const [curr, setCurr] = useState(0)
+
+//   const prev = () =>
+//     setCurr((curr) => (curr === 0 ? slides.length - 1 : curr - 1))
+//   const next = () =>
+//     setCurr((curr) => (curr === slides.length - 1 ? 0 : curr + 1))
+
+//   useEffect(() => {
+//     if (!autoSlide) return
+//     const slideInterval = setInterval(next, autoSlideInterval)
+//     return () => clearInterval(slideInterval)
+//   }, [])
+//   return (
+//     <div className="overflow-hidden relative">
+//       <div className="flex transition-transform ease-out duration-500" style={{ transform: `translateX(-${curr * 100}%)` }} >
+//         {slides}
+//       </div>
+
+//       <div className="absolute inset-0 flex items-center justify-between p-1">
+//         <button onClick={prev} className="p-1  rounded-full shadow bg-black/80 text-white hover:bg-black  hover:drop-shadow-lg hover:scale-110 duration-500" >
+//           <BsChevronCompactLeft size={60} />
+//         </button>
+
+//         <button onClick={next}  className="p-1 rounded-full shadow bg-black/80 text-white hover:bg-black  hover:drop-shadow-lg hover:scale-110 duration-500">
+//         <BsChevronCompactRight size={60} />
+//         </button>
+//       </div>
+
+//       <div className="absolute bottom-4 right-0 left-0">
+//         <div className="flex items-center justify-center gap-2">
+//           {slides.map((_, i) => (
+//             <div key={i} className={`transition-all w-3 h-3 bg-black rounded-full hover:scale-110  ${curr === i ? "p-2" : "bg-opacity-50"}`}/>
+//           ))}
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Carousel
