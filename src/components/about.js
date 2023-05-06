dc78e1bdb738716a8c531a67adf78ffd4de1595b
Hithaish <shetty9606@gmail.com>
2023-05-07 00:40:47 +0530

    deploy trial 5

--- src/components/about.js
+++ src/components/about.js
@@ -35,8 +35,8 @@ const About=()=> {
 
             </div>
 
-            <div className="max-w-[900px] h-full text-base md:text-xl px-9 md:pl-20 md:text-black  text-black dark:text-white md:dark:text-white">
-                <p className='text-justify font-[470]'>Hello guys, welcome to my portfolio! I am a frontend Developer engineering student, interested in developing web applications and exploring new technologies. Currently I'm working on reactjs projects for learning</p>
+            <div className="max-w-[900px] h-full text-base md:text-xl px-9 md:pl-20">
+                <p className='text-justify  md:text-black  text-black dark:text-white md:dark:text-white font-[470]'>Hello guys, welcome to my portfolio! I am a frontend Developer engineering student, interested in developing web applications and exploring new technologies. Currently I'm working on reactjs projects for learning</p>
                 <ul className='text-base md:text-black text-black md:dark:text-gray-400 dark:text-gray-400 md:text-xl pt-5 flex space-x-8 font-medium cursor-pointer uppercase w-full'>
                   <li className={`navelements  ${selected === 'skills' ? 'active-navelements' : ''}`} onClick={() => handleClick('skills')}>Skills</li>
                   <li className={`navelements  ${selected === 'education' ? 'active-navelements' : ''}`}  onClick={() => handleClick('education')}>Education</li>
@@ -44,7 +44,7 @@ const About=()=> {
                 </ul>
 
 
-                <div className='mainContainer w-screen md:w-full h-fit md:h-[310px] p-5'>
+                <div className='mainContainer w-screen md:w-full h-[310px] p-5'>
                     
                     {selected === 'skills' && (
                       <div name="skills"  className={` content active-content flex w-full h-full md:pt-0 -translate-x-14 `}>
@@ -61,7 +61,7 @@ const About=()=> {
                     
                     {selected=== 'education' && (
                       <div name='education' className=' md:text-black md:dark:text-white text-black dark:text-white w-full h-full'>
-                        <ol className='list-disc flex flex-col space-y-5 font-[470]'>
+                        <ol className='list-disc flex flex-col space-y-5 font-[470] pr-5'>
                           <li><span className='font-medium'>Graduating 2023</span><span className='flex'>B.E IN COMPUTER SCIENCE AND ENGINEERING &nbsp; | &nbsp; CGPA: 8.03</span></li>
                           <li><span className='font-medium'>Graduated 2019</span><span className='flex flex-col'>PUC &nbsp; | &nbsp; Percentage: 89.5</span></li>
                           <li><span className='font-medium'>Graduated 2017</span><span className='flex flex-col'>SSLC &nbsp; | &nbsp; Percentage: 84.96</span></li>
@@ -72,7 +72,7 @@ const About=()=> {
 
                     {selected === 'experience' && (
                       <div name='experience' className=' md:text-black md:dark:text-white text-black dark:text-white w-full h-full '>
-                        <ol className='list-none font-[470] space-y-1 text-justify'>
+                        <ol className='list-none font-[470] space-y-1 text-justify pr-14'>
                         <p className='font-semibold py-1'> Full Stack Developer Intern (Aug 2022-Sep 2022)</p>
                         <li className='list-disc'>I have done one month internship at <a className='font-medium underline' href='https://thaniyatech.com/'>Thaniya Technologies IT Solutions LLP</a>, Mangaluru.</li>
                         <li className='list-disc'>One of the most valuable skills I had learned are project planning & management and the ability to speak with people in a professional setting.</li>
