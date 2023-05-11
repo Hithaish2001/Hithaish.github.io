d165b701726361315903fabbed80d6e92cfb77d3
Hithaish <shetty9606@gmail.com>
2023-05-11 23:45:11 +0530

    modifying project section 2

--- src/components/projects.js
+++ src/components/projects.js
@@ -25,10 +25,10 @@ const Project=()=>{
 
             <div className="w-full text-center"><p className='text-5xl md:text-6xl font-bold font-mono pb-14 md:text-black text-black md:dark:text-white dark:text-white'>PROJECTS</p></div>
             
-            <div className=' w-[90%] h-2/3 grid grid-cols-1 pt-5 md:pt-0 justify-items-center md:grid-cols-3 gap-8 bg-gray-700 md:bg-transparent border-2 md:border-none rounded-md overflow-auto'>
+            <div className=' w-[90%] md:h-2/3 h-[700px] grid grid-cols-1 py-6 md:py-0 justify-items-center md:grid-cols-3 gap-8 bg-gray-700 md:bg-transparent border-2 md:border-none rounded-md overflow-hidden overflow-y-scroll md:overflow-auto scroll-my-6'>
               {images.map((items)=>(
                 
-                <div key={items.id} className='w-[90%] h-full relative rounded-md group overflow-hidden cursor-pointer hover:shadow-xl dark:hover:shadow-lg hover:shadow-gray-400 dark:hover:shadow-amber-100 ring-1 ring-black'>
+                <div key={items.id} className='w-[90%] md:w-full h-[250px] md:h-full relative rounded-md group overflow-hidden cursor-pointer hover:shadow-xl dark:hover:shadow-lg hover:shadow-gray-400 dark:hover:shadow-amber-100 ring-1 ring-black'>
                   
                     
                       <img src={items.images} alt="" className='w-full h-full rounded-md opacity-100 group-hover:opacity-75 relative z-10 scale-100 group-hover:scale-105 transition-all duration-500 ease-out origin-top object-cover'/>
